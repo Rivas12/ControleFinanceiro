51cbb4b2e8f3fb6bb50447e0e0343143a32d9799
Rivas12 <rivaldosilveira13@gmail.com>
2023-07-25 23:55:29 -0300

    menu de opções criado

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from entities.functions import Functions
 
+#Função responsavel pelo menu, assim que o usuario escolher a opção a função será executada
 stop = False
-
 while not stop:
     menu = input('''
  Selecione a opção desejada: 
