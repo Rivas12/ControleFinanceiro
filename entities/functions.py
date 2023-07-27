e15c7be7fef8a0928d77a4952f80141dd2159707
Rivas12 <rivaldosilveira13@gmail.com>
2023-07-27 10:04:33 -0300

    novas variaveis criadas para receberem os dados

--- entities/functions.py
+++ entities/functions.py
@@ -3,23 +3,23 @@ import os
 def CadastrarEntrada():
     os.system('cls' if os.name == 'nt' else 'clear') # limpar terminal
     print("Preencha os dados da nova entrada: \n")
-    input("Nome: ")
-    input("Valor: ")
-    input("Categoria: ")
-    input("Data: ")
+    nome = input("Nome da entrada: ")
+    valor = input("Valor: ")
+    categoria = input("Categoria: ")
+    data = input("Data: ")
 
 def CadastrarSaida():
     os.system('cls' if os.name == 'nt' else 'clear') # limpar terminal
     print("Preencha os dados da nova saída: \n")
-    input("Nome: ")
-    input("Valor: ")
-    input("Categoria: ")
-    input("Data: ")
+    nome = input("Nome da saída: ")
+    valor = input("Valor: ")
+    categoria = input("Categoria: ")
+    data = input("Data: ")
 
 def CadastrarInvestimento():
     os.system('cls' if os.name == 'nt' else 'clear') # limpar terminal
     print("Preencha os dados do novo investimento: \n")
-    input("Nome: ")
-    input("Valor: ")
-    input("Categoria: ")
-    input("Data: ")
+    nome = input("Nome do investimento: ")
+    valor = input("Valor: ")
+    tipo = input("tipo: ")
+    data = input("Data: ")
