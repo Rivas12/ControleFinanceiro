191de02c1c7efe01e51d2cb39bfe4198392bc9b4
Rivas12 <rivaldosilveira13@gmail.com>
2023-07-28 17:52:19 -0300

    função limpar terminal criada para facilitar visualização do codigo

--- entities/functions.py
+++ entities/functions.py
@@ -1,11 +1,14 @@
 import os 
 import sqlite3
 
+def LimparTerminal(): # limpar terminal
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 con = sqlite3.connect("db.db") #conexão com o banco de dados
 cursor = con.cursor()
 
 def CadastrarEntrada():
-    os.system('cls' if os.name == 'nt' else 'clear') # limpar terminal
+    LimparTerminal()
     print("Preencha os dados da nova entrada: \n")
     nome = input("Nome da entrada: ")
     valor = input("Valor: ")
@@ -14,19 +17,22 @@ def CadastrarEntrada():
 
     cursor.execute(f'INSERT INTO "main"."Entradas" ("nome", "valor", "categoria", "data") VALUES ("{nome}", "{valor}", "{categoria}", "{data}");')
     con.commit()
+    LimparTerminal()
 
 def CadastrarSaida():
-    os.system('cls' if os.name == 'nt' else 'clear') # limpar terminal
+    LimparTerminal()
     print("Preencha os dados da nova saída: \n")
     nome = input("Nome da saída: ")
     valor = input("Valor: ")
     categoria = input("Categoria: ")
     data = input("Data: ")
+    LimparTerminal()
 
 def CadastrarInvestimento():
-    os.system('cls' if os.name == 'nt' else 'clear') # limpar terminal
+    LimparTerminal()
     print("Preencha os dados do novo investimento: \n")
     nome = input("Nome do investimento: ")
     valor = input("Valor: ")
     tipo = input("tipo: ")
     data = input("Data: ")
+    LimparTerminal()
