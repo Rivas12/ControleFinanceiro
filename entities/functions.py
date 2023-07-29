00eea718674cbf087a2b4524cf712f0c705732a1
Rivas12 <rivaldosilveira13@gmail.com>
2023-07-29 10:37:57 -0300

    novas funções como mostrar db agora funcionam

--- entities/functions.py
+++ entities/functions.py
@@ -1,5 +1,6 @@
 import os 
 import sqlite3
+from prettytable import PrettyTable
 
 def LimparTerminal(): # limpar terminal
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -8,7 +9,6 @@ con = sqlite3.connect("db.db") #conexão com o banco de dados
 cursor = con.cursor()
 
 def CadastrarEntrada():
-    LimparTerminal()
     #capturar os dados da Entrada:
     print("Preencha os dados da nova entrada: \n")
     nome = input("Nome da entrada: ")
@@ -19,10 +19,8 @@ def CadastrarEntrada():
 
     cursor.execute(f'INSERT INTO "main"."Entradas" ("nome", "valor", "categoria", "data") VALUES ("{nome}", "{valor}", "{categoria}", "{data}");')
     con.commit() #salvar no banco de dados
-    LimparTerminal()
 
 def CadastrarSaida():
-    LimparTerminal()
     #capturar os dados da saida:
     print("Preencha os dados da nova saída: \n")
     nome = input("Nome da saída: ")
@@ -33,11 +31,8 @@ def CadastrarSaida():
 
     cursor.execute(f'INSERT INTO "main"."Saidas" ("nome", "valor", "categoria", "data") VALUES ("{nome}", "{valor}", "{categoria}", "{data}");')
     con.commit() #salvar no banco de dados
-    LimparTerminal()
 
 def CadastrarInvestimento():
-    
-    LimparTerminal()
     #capturar os dados do investimento:
     print("Preencha os dados do novo investimento: \n")
     nome = input("Nome do investimento: ")
@@ -49,16 +44,53 @@ def CadastrarInvestimento():
 
     cursor.execute(f'INSERT INTO "main"."Investimentos" ("nome", "valor", "tipo", "quantidade", "data") VALUES ("{nome}", "{valor}", "{tipo}", "{qtd}", "{data}");')
     con.commit() #salvar no banco de dados
-    LimparTerminal()
 
 def GerarRelatorio():
-    LimparTerminal()
-    print("Mostrando dados: \n")
-    cursor.execute("SELECT SUM(valor) FROM Entradas")
-    print(f"Entrada: R${cursor.fetchall()[0][0]}")
-    cursor.execute("SELECT SUM(valor) FROM Saidas")
-    print(f"Saídas: R${cursor.fetchall()[0][0]}")
-    cursor.execute("SELECT SUM(valor) FROM Investimentos")
-    print(f"Investimentos: R${cursor.fetchall()[0][0]}")
-    input("\n\nprecione alguma tecla para sair")
+    menu = input('''
+ Oque deseja fazer?
+                 
+ 1: Mostrar todas as entradas 
+ 2: Mostrar todas as saidas
+ 3: Mostrar todos os investimentos \n
+ 4: Mostrar a soma de todos
+ → ''')
+    if menu == "1":
+        LimparTerminal()
+        cursor.execute("SELECT * FROM Entradas")
+        MyTableEntrada = PrettyTable(["ID", "Nome", "Valor", "Categoria", "Data"])
+        data = cursor.fetchall()
+        for i in data: 
+            MyTableEntrada.add_row([i[0], i[1], i[2], i[3], i[4]])
+        print("Dados da tabela entrada: \n")
+        print(MyTableEntrada)
+        input("\n\nprecione alguma tecla para sair ")
+    if menu == "2":
+        LimparTerminal()
+        cursor.execute("SELECT * FROM Saidas")
+        MyTableEntrada = PrettyTable(["ID", "Nome", "Valor", "Categoria", "Data"])
+        data = cursor.fetchall()
+        for i in data: 
+            MyTableEntrada.add_row([i[0], i[1], i[2], i[3], i[4]])
+        print("Dados da tabela saídas: \n")
+        print(MyTableEntrada)
+        input("\n\nprecione alguma tecla para sair ")
+    if menu == "3":
+        LimparTerminal()
+        cursor.execute("SELECT * FROM Investimentos")
+        MyTableEntrada = PrettyTable(["ID", "Nome", "Valor", "Tipo", "Quantidade", "Data"])
+        data = cursor.fetchall()
+        for i in data: 
+            MyTableEntrada.add_row([i[0], i[1], i[2], i[3], i[4], i[5]])
+        print("Dados da tabela saídas: \n")
+        print(MyTableEntrada)
+        input("\n\nprecione alguma tecla para sair ")
+    if menu == "4":
+        LimparTerminal()
+        cursor.execute("SELECT SUM(valor) FROM Entradas")
+        print(f"Entrada: R${cursor.fetchall()[0][0] if cursor.fetchall()[0][0] == None else '0'}")
+        cursor.execute("SELECT SUM(valor) FROM Saidas")
+        print(f"Saídas: R${cursor.fetchall()[0][0] if cursor.fetchall()[0][0] != None else '0'}")
+        cursor.execute("SELECT SUM(valor) FROM Investimentos")
+        print(f"Investimentos: R${cursor.fetchall()[0][0] if cursor.fetchall()[0][0] != None else '0'}")
+        input("\n\nprecione alguma tecla para sair ")
     LimparTerminal()
--- main.py
+++ main.py
@@ -3,22 +3,23 @@ from entities.functions import *
 #Função responsavel pelo menu, assim que o usuario escolher a opção a função será executada
 stop = False
 while not stop:
-    menu = input('''
+    menus = input('''
  Selecione a opção desejada: 
                  
  1: Criar entrada
  2: Criar saída
  3: Criar investimento \n
- 4: Relatorio \n
- 5: sair
+ 4: Relatorio
  → ''')
-    if menu == "1":
+    LimparTerminal()
+    if menus == "1":
         CadastrarEntrada()
-    if menu == "2":
+    if menus == "2":
         CadastrarSaida()
-    if menu == "3":
+    if menus == "3":
         CadastrarInvestimento()
-    if menu == "4":
+    if menus == "4":
         GerarRelatorio()
-    if menu == "5":
+    if menus == "5":
         break
+    LimparTerminal()
