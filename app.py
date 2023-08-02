19e4717985ed93bb8451538f60745a5609a53d78
Rivas12 <rivaldosilveira13@gmail.com>
2023-08-02 12:02:36 -0300

    todo banco de dados é exibido na pagina web e também podem ser salvo os dados direto da pagina p o banco de dados

--- app.py
+++ app.py
@@ -8,20 +8,41 @@ cursor = con.cursor()
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return render_template('entrada.html')
+@app.route('/entradas')
+def entrada():
+    cursor.execute("SELECT * FROM Entradas")
+    data = cursor.fetchall()
+    return render_template('index.html', data = data, page = "entrada")
 
-@app.route('/cadastrar', methods = ['POST'])
-def cadastrar():
-    print(request.form["nome"])
-    print(request.form["valor"])
-    print(request.form["data"])
-    print(request.form["categoria"])
+@app.route('/saidas')
+def saida():
+    cursor.execute("SELECT * FROM Saidas")
+    data = cursor.fetchall()
+    return render_template('index.html', data = data, page = "saida")
+
+@app.route('/investimentos')
+def investimentos():
+    cursor.execute("SELECT * FROM Investimentos")
+    data = cursor.fetchall()
+    return render_template('index.html', data = data, page = "investimento")
+
+@app.route('/cadastrar/entrada', methods = ['POST'])
+def cadastrarEntrada():
     cursor.execute(f'INSERT INTO "main"."Entradas" ("nome", "valor", "categoria", "descricao", "data") VALUES ("{request.form["nome"]}", "{request.form["valor"]}", "{request.form["categoria"]}", "{request.form["descricao"]}", "{request.form["data"]}");')
     con.commit() #salvar no banco de dados
-    return redirect("http://localhost:5000/")
+    return redirect("http://localhost:5000/entradas")
 
+@app.route('/cadastrar/saida', methods = ['POST'])
+def cadastrarSaida():
+    cursor.execute(f'INSERT INTO "main"."Saidas" ("nome", "valor", "categoria", "descricao", "data") VALUES ("{request.form["nome"]}", "{request.form["valor"]}", "{request.form["categoria"]}", "{request.form["descricao"]}", "{request.form["data"]}");')
+    con.commit() #salvar no banco de dados
+    return redirect("http://localhost:5000/saidas")
+
+@app.route('/cadastrar/investimento', methods = ['POST'])
+def cadastrarInvestimento():
+    cursor.execute(f'INSERT INTO "main"."Investimentos" ("nome", "valor", "categoria", "descricao", "data") VALUES ("{request.form["nome"]}", "{request.form["valor"]}", "{request.form["categoria"]}", "{request.form["descricao"]}", "{request.form["data"]}");')
+    con.commit() #salvar no banco de dados
+    return redirect("http://localhost:5000/investimentos")
 
 if __name__ == "__main__":
     app.run(debug=True)
