157c80989763ab108dcd6b143c5169931ffc796f
Rivas12 <rivaldosilveira13@gmail.com>
2023-08-06 18:25:55 -0300

    modificações no banco de dados e metodo de salvar

--- app.py
+++ app.py
@@ -7,25 +7,31 @@ cursor = con.cursor()
 
 app = Flask(__name__)
 
-@app.route('/movimentacoes')
+@app.route('/')
 def home():
-    return render_template('index.html', page = "movimentacoes")
+    return redirect('http://localhost:5000/movimentacoes')
+
+@app.route('/movimentacoes')
+def movimentacoes():
+    cursor.execute("SELECT * FROM Movimentacoes ORDER BY data DESC")
+    data = cursor.fetchall()
+    return render_template('index.html', page = "movimentacoes", contador = 0, data = data)
 
 @app.route('/entradas')
 def entrada():
-    cursor.execute("SELECT * FROM Entradas")
+    cursor.execute('SELECT * FROM Movimentacoes WHERE tipo == "entrada"')
     data = cursor.fetchall()
     return render_template('index.html', data = data, page = "entrada")
 
 @app.route('/saidas')
 def saida():
-    cursor.execute("SELECT * FROM Saidas")
+    cursor.execute('SELECT * FROM Movimentacoes WHERE tipo == "saida"')
     data = cursor.fetchall()
     return render_template('index.html', data = data, page = "saida")
 
 @app.route('/investimentos')
 def investimentos():
-    cursor.execute("SELECT * FROM Investimentos")
+    cursor.execute('SELECT * FROM Movimentacoes WHERE tipo == "investimento"')
     data = cursor.fetchall()
     return render_template('index.html', data = data, page = "investimento")
 
@@ -35,21 +41,27 @@ def relatorio():
 
 @app.route('/cadastrar/entrada', methods = ['POST'])
 def cadastrarEntrada():
-    cursor.execute(f'INSERT INTO "main"."Entradas" ("nome", "valor", "categoria", "descricao", "data") VALUES ("{request.form["nome"]}", "{request.form["valor"]}", "{request.form["categoria"]}", "{request.form["descricao"]}", "{request.form["data"]}");')
+    cursor.execute(f'INSERT INTO "main"."Movimentacoes" ("nome", "tipo", "valor", "categoria", "descricao", "data") VALUES ("{request.form["nome"]}", "entrada", "{request.form["valor"]}", "{request.form["categoria"]}", "{request.form["descricao"]}", "{request.form["data"]}");')
     con.commit() #salvar no banco de dados
     return redirect("http://localhost:5000/entradas")
 
 @app.route('/cadastrar/saida', methods = ['POST'])
 def cadastrarSaida():
-    cursor.execute(f'INSERT INTO "main"."Saidas" ("nome", "valor", "categoria", "descricao", "data") VALUES ("{request.form["nome"]}", "{request.form["valor"]}", "{request.form["categoria"]}", "{request.form["descricao"]}", "{request.form["data"]}");')
+    cursor.execute(f'INSERT INTO "main"."Movimentacoes" ("nome", "tipo", "valor", "categoria", "descricao", "data") VALUES ("{request.form["nome"]}", "saida", "{request.form["valor"]}", "{request.form["categoria"]}", "{request.form["descricao"]}", "{request.form["data"]}");')
     con.commit() #salvar no banco de dados
     return redirect("http://localhost:5000/saidas")
 
 @app.route('/cadastrar/investimento', methods = ['POST'])
 def cadastrarInvestimento():
-    cursor.execute(f'INSERT INTO "main"."Investimentos" ("nome", "valor", "categoria", "descricao", "data") VALUES ("{request.form["nome"]}", "{request.form["valor"]}", "{request.form["categoria"]}", "{request.form["descricao"]}", "{request.form["data"]}");')
+    cursor.execute(f'INSERT INTO "main"."Movimentacoes" ("nome", "tipo", "valor", "categoria", "descricao", "data") VALUES ("{request.form["nome"]}", "investimento", "{request.form["valor"]}", "{request.form["categoria"]}", "{request.form["descricao"]}", "{request.form["data"]}");')
     con.commit() #salvar no banco de dados
     return redirect("http://localhost:5000/investimentos")
+
+@app.route('/deletar', methods = ['GET'])
+def deletar():
+    cursor.execute(f'DELETE FROM Movimentacoes WHERE id == {request.args["id"]}')
+    con.commit()
+    return redirect("http://localhost:5000/movimentacoes")
 
 if __name__ == "__main__":
     app.run(debug=True)
